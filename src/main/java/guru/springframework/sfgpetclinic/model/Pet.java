b7885495c854a2548e61b1e6ff1f7d876564cae2
Hanno.vdWalt <hanno>
2021-03-08 13:20:26 +0200

    Added POJO Pushing

--- src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -2,7 +2,7 @@ package guru.springframework.sfgpetclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity {
     private petType petType;
     private Owner owner;
     private LocalDate birthDate;
