d880ae94c85ce8e40c2ebc73de84dab626486a9e
Hanno.vdWalt <hanno>
2021-03-08 08:37:04 +0200

    Added POJO

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -0,0 +1,33 @@
+package guru.springframework.sfgpetclinic.model;
+
+import java.time.LocalDate;
+
+public class Pet {
+    private petType petType;
+    private Owner owner;
+    private LocalDate birthDate;
+
+    public guru.springframework.sfgpetclinic.model.petType getPetType() {
+        return petType;
+    }
+
+    public void setPetType(guru.springframework.sfgpetclinic.model.petType petType) {
+        this.petType = petType;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+}
